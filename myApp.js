b7b7bb1dcdbe6e338909f1c67ee2c868319d4cfe
Pablo Pereira <vinicius7pablo@gmail.com>
2023-03-13 21:31:21 -0300

    Update

--- myApp.js
+++ myApp.js
@@ -1,19 +1,21 @@
 let express = require('express');
 let app = express();
 
+viewsPath = __dirname + '/views/index.html'
+app.get('/', function(req, res) {
+    res.sendFile(viewsPath);
+});
 
+publicPath = __dirname + '/public';
+app.use('/public', express.static(publicPath));
 
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"});
+});
 
-
-
-
-
-
-
-
-
-
-
+app.listen(3001, function() {
+    console.log("Listening on 3001.");
+});
 
 
 
